1b8ee12b4617bab1284f68f15f12237a8807b725
nicolas blum <nblumferracci@gmail.com>
2021-02-23 14:25:47 +0100

    animation and left right movement

--- floor.js
+++ floor.js
@@ -1,10 +1,11 @@
 const floorArrays = [];
+let hue = 0;
 
 class Floor {
   constructor() {
     this.height = (canvas.height * 10) / 100;
     this.color = "hsla(" + hue + ",100%,50%,0.8)";
-    this.width = canvas.width / 20;
+    this.width = canvas.width;
     this.x = 0;
   }
   draw() {
@@ -26,8 +27,9 @@ function handleFloor() {
     floorArrays[i].update();
   }
   floorArrays.map((floor) => (floor.x += floor.width));
+  hue++;
 
-  if (floorArrays.length > 25) {
+  if (floorArrays.length > 3) {
     floorArrays.pop(floorArrays[0]);
   }
 }
--- main.js
+++ main.js
@@ -1,24 +1,125 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 
-let hue = 0;
-// let gameSpeed = 0;
-// let frame = 0;
+// set the canvas size
+canvas.width = 1280;
+canvas.height = 720;
 
-// console.log(ctx);
-canvas.width = 900;
-canvas.height = 900;
+const keys = [];
 
-function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  // floor.draw();
-  handleFloor();
-  requestAnimationFrame(animate);
-  hue++;
+const playerSprite = new Image();
+playerSprite.src = "Cuphead.png";
+
+const background = new Image();
+background.src = "background.png";
+let backgroundPositionX = 0;
+
+const player = {
+  x: 0,
+  y: canvas.height - playerSprite.height / 8 - 150,
+  width: playerSprite.width / 16,
+  height: playerSprite.height / 8,
+  frameX: 0,
+  frameY: 5,
+  speed: 9,
+  moving: false,
+};
+
+function drawCuphead(img, sX, sY, sW, sH, dX, dY, dW, dH) {
+  ctx.drawImage(img, sX, sY, sW, sH, dX, dY, dW, dH);
 }
-animate();
 
-window.addEventListener("resize", function () {
-  canvas.height = window.innerHeight;
-  canvas.width = window.innerWidth;
+// function animateCanvas() {
+//   ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   ctx.drawImage(
+//     background,
+//     backgroundPositionX,
+//     0,
+//     canvas.width,
+//     canvas.height
+//   );
+//   drawCuphead(
+//     playerSprite,
+//     player.width * player.frameX,
+//     player.height * player.frameY,
+//     player.width,
+//     player.height,
+//     player.x,
+//     player.y,
+//     player.width,
+//     player.height
+//   );
+//   // backgroundPositionX++; this could move the background
+//   movePlayer();
+//   handleMove();
+//   requestAnimationFrame(animateCanvas);
+// }
+// animateCanvas();
+
+// add event listener on keyboard
+window.addEventListener("keydown", function (e) {
+  keys[e.key] = true;
+  player.moving = true;
+  // console.log(e);
 });
+window.addEventListener("keyup", function (e) {
+  delete keys[e.key];
+  player.moving = false;
+});
+
+function movePlayer() {
+  if (keys["ArrowLeft"] && player.x > 0) {
+    player.x -= player.speed;
+    player.moving = true;
+    player.frameY = 4;
+  }
+  if (keys["ArrowRight"] && player.x < canvas.width - player.width) {
+    player.x += player.speed;
+    player.moving = true;
+    player.frameY = 3;
+  }
+}
+
+function handlePlayerFrame() {
+  if (player.frameX < 13 && player.moving) player.frameX++;
+  else player.frameX = 4;
+}
+
+let fps, fpsInterval, startTime, now, then, elapsed;
+
+function startAnimating(fps) {
+  fpsInterval = 1000 / fps;
+  then = Date.now();
+  animateCanvas();
+}
+
+function animateCanvas() {
+  requestAnimationFrame(animateCanvas);
+  now = Date.now();
+  elapsed = now - then;
+  if (elapsed > fpsInterval) {
+    then = now - (elapsed % fpsInterval);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(
+      background,
+      backgroundPositionX,
+      0,
+      canvas.width,
+      canvas.height
+    );
+    drawCuphead(
+      playerSprite,
+      player.width * player.frameX,
+      player.height * player.frameY,
+      player.width,
+      player.height,
+      player.x,
+      player.y,
+      player.width,
+      player.height
+    );
+    movePlayer();
+    handlePlayerFrame();
+  }
+}
+startAnimating(30);
